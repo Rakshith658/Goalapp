a4dcbb20f97a50346d55f938ad93ca1aa1b8d4ae
Rakshith Kumar S <rakshithkumars7777@gmail.com>
2020-12-26 23:00:05 +0530

    added a flstlist

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { Button, FlatList, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [gaol, setgaol] = useState('');
+  const [crouseGoal,setcrouseGoal] = useState([]);
+
+  const goalIputHandlear = (enteredtext) =>{
+    setgaol(enteredtext)
+  }
+
+  const addGaolHandeler = () => {
+    setcrouseGoal(currentGoal=>[...currentGoal,{id : Math.random().toString(),value:gaol}])
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputview}>
+        <TextInput
+        placeholder="Goal's"
+        style={styles.inputtext}
+        onChangeText={goalIputHandlear}
+        value={gaol}
+         />
+        <Button 
+        title='Add'
+        onPress={addGaolHandeler}
+        style={{borderRadius:10}}
+        />
+      </View>
+      <FlatList data={crouseGoal}
+      keyExtractor={(item ,index)=>item.id} 
+      renderItem={(Goal)=>
+      <View style={styles.listitem} key={Goal} >
+        <Text style={styles.text}>{Goal.item.value}</Text>
+      </View>}
+      />
+        
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding:50,
+    backgroundColor: '#fff'
   },
+  inputview:{
+    flexDirection:"row",
+    justifyContent:"space-between",
+    alignItems:'center'
+  },
+  inputtext:{
+    width:'80%',
+    borderColor:'black',
+    borderBottomWidth:1,
+    padding:10,
+  },
+  listitem : {
+    marginTop:10,
+    padding:10,
+    backgroundColor:'#009387',
+    borderColor:'black',
+    borderWidth:1,
+    borderRadius:10,
+  },
+  text : {
+    color:'#fff'
+  }
 });
