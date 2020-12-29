292e5d6282b513751e0d14d29bc3362905f04823
Rakshith Kumar S <rakshithkumars7777@gmail.com>
2020-12-29 15:18:19 +0530

    all ok

---
+++ components/GoalItems.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { View, Text,StyleSheet } from 'react-native'
+
+const GoalItems = (props) => {
+    return (
+        <View style={styles.listitem} key={Goal} >
+        <Text style={styles.text}>{props.title}</Text>
+      </View>
+    )
+}
+
+export default GoalItems;
+
+const styles = StyleSheet.create({
+
+    listitem : {
+        marginTop:10,
+        padding:10,
+        backgroundColor:'#009387',
+        borderColor:'black',
+        borderWidth:1,
+        borderRadius:10,
+    },
+    text : {
+        color:'#fff'
+    }
+})
